a434c9b16ba9fba2e53f2c5ba12604e4f9ebe051
Ondrej Krajcik <o.krajcik@seznam.cz>
2016-01-17 13:41:40 +0100

    Autenticator: add authenticator

--- src/BasePresenterTester.php
+++ src/BasePresenterTester.php
@@ -2,7 +2,15 @@
 
 namespace Helbrary\NetteTesterExtension;
 
+use Nette\Configurator;
+use Nette\Object;
+use Nette\Security\AuthenticationException;
+use Nette\Security\IAuthenticator;
+use Nette\Security\Identity;
+use Nette\Security\IIdentity;
 use Tester\Assert;
+use Tracy\Debugger;
+
 
 abstract class BasePresenterTester extends \Tester\TestCase
 {
@@ -22,11 +30,16 @@ abstract class BasePresenterTester extends \Tester\TestCase
 	 */
 	protected $presenterFactory;
 
+	/** @var  Authenticator */
+	protected $authenticator;
+
 	public function __construct()
 	{
 		$this->container = require __DIR__ . '/../../../../app/bootstrap.php';
 		$this->linkGenerator = $this->container->getByType('Nette\Application\LinkGenerator');
 		$this->presenterFactory = $this->container->getByType('Nette\Application\IPresenterFactory');
+		$this->authenticator = new Authenticator();
+		$configurator = new Configurator();
 	}
 
 	/**
@@ -47,11 +60,16 @@ abstract class BasePresenterTester extends \Tester\TestCase
 	 * @param string $presenterName - etc. 'Front:GoodsChange:Goods'
 	 * @param string $method
 	 * @param array $parameters
+	 * @param null|int $userId
 	 * @return \Nette\Application\IResponse
 	 */
-	public function sendRequest($presenterName, $method = 'GET', $parameters = array())
+	public function sendRequest($presenterName, $method = 'GET', $parameters = array(), $userId = NULL)
 	{
 		$presenter = $this->getPresenter($presenterName);
+		if ($userId !== NULL) {
+			$presenter->user->setAuthenticator($this->authenticator);
+			$presenter->user->login($userId, '');
+		}
 		$request = new \Nette\Application\Request($presenterName, $method, $parameters);
 		/** @var $response \Nette\Application\Responses\RedirectResponse */
 		return $presenter->run($request);
@@ -62,11 +80,12 @@ abstract class BasePresenterTester extends \Tester\TestCase
 	 * @param string $presenterName - etc. 'Front:GoodsChange:Goods'
 	 * @param string $method
 	 * @param array $parameters
+	 * @param null|int $userId
 	 */
-	public function checkRequestNoError($presenterName, $method = 'GET', $parameters = array())
+	public function checkRequestNoError($presenterName, $method = 'GET', $parameters = array(), $userId = NULL)
 	{
-		Assert::noError(function() use ($presenterName, $method, $parameters) {
-			$this->sendRequest($presenterName, $method, $parameters);
+		Assert::noError(function() use ($presenterName, $method, $parameters, $userId) {
+			$this->sendRequest($presenterName, $method, $parameters, $userId);
 		});
 	}
 
@@ -76,12 +95,30 @@ abstract class BasePresenterTester extends \Tester\TestCase
 	 * @param string $method
 	 * @param array $parameters
 	 * @param string $expectedType
+	 * @param null|int $userId
 	 * @throws \Exception
 	 */
-	public function checkRequestError($presenterName, $method = 'GET', $parameters = array(), $expectedType)
+	public function checkRequestError($presenterName, $method = 'GET', $parameters = array(), $expectedType, $userId = NULL)
 	{
-		Assert::Error(function() use ($presenterName, $method, $parameters) {
-			$this->sendRequest($presenterName, $method, $parameters);
+		Assert::Error(function() use ($presenterName, $method, $parameters, $userId) {
+			$this->sendRequest($presenterName, $method, $parameters, $userId);
 		}, $expectedType);
+	}
+}
+
+class Authenticator extends Object implements IAuthenticator
+{
+
+	/**
+	 * Performs an authentication against e.g. database.
+	 * and returns IIdentity on success or throws AuthenticationException
+	 * @param array $credentials
+	 * @return IIdentity
+	 * @throws AuthenticationException
+	 */
+	function authenticate(array $credentials)
+	{
+		list( $id, $password ) = $credentials;
+		return new Identity( $id, 'admin', NULL );
 	}
 }

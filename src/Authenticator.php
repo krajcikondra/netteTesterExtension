af61412a2ac555ee958f986ae7883efe31776f5f
krajcikondra <o.krajcik@seznam.cz>
2019-01-12 02:02:13 +1300

    Authenticator: add method setIdentityData

--- src/Authenticator.php
+++ src/Authenticator.php
@@ -13,6 +13,9 @@ class Authenticator implements IAuthenticator
 
 	use SmartObject;
 
+	/** @var array|null */
+	protected $identityData;
+
 	/**
 	 * Performs an authentication against e.g. database.
 	 * and returns IIdentity on success or throws AuthenticationException
@@ -23,6 +26,17 @@ class Authenticator implements IAuthenticator
 	function authenticate(array $credentials)
 	{
 		list( $id, $role ) = $credentials;
-		return new Identity( $id, $role, NULL );
+		return new Identity( $id, $role, $this->identityData );
 	}
+
+
+    /**
+     * @param array|NULL $data
+     * @return self
+     */
+	public function setIdentityData(array $data = NULL) {
+	    $this->identityData = $data;
+	    return $this;
+    }
+
 }
